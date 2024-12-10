39cd88475ec77bf0a0b53f4283127efc28e3c359
alvinrach <alvinracht@gmail.com>
2024-12-10 22:37:06 +0700

    New Automation Strategy
    Co-authored-by: Alvio Yunita Putri <alvio.yunita1997@gmail.com>

--- automation2.py
+++ automation2.py
@@ -12,8 +12,38 @@ time.sleep(10)  # Wait for WhatsApp Web to load
 pyautogui.hotkey('ctrl', '0')
 
 # Function to send WhatsApp message
-def send_whatsapp_message(name, phone):
-    message = f"Do you mind, {name}!"
+def send_whatsapp_message(name, title, phone):
+    encoded_name = urllib.parse.quote(name)
+    message = (
+        f"Kepada Yth. \n"
+        f"{title}\n"
+        f"{name}\n"
+        f"____\n\n"
+        "‎اللهِ الرَّحْمَنِ الرَّحِيْم‎‎ بِسْــــــــــــــــــمِ\n\n"
+        "Assalamu'alaikum Warahmatullahi Wabarakaatuh\n\n"
+        "Tanpa mengurangi rasa hormat, perkenankan kami mengundang Bapak/Ibu/Saudara/i, teman sekaligus sahabat, "
+        "untuk menghadiri acara pernikahan kami:\n\n"
+        "Alvio Yunita Putri, S.T\n"
+        "&\n"
+        "Alvin Rachmat, S.T\n\n"
+        "yang InsyaAllah akan diselenggarakan pada :\n\n"
+        "Akad & Resepsi di Palembang\n"
+        "Hari, tanggal    : Minggu, 27 Oktober 2024\n"
+        "Waktu            : 10:00 WIB s.d. Selesai\n"
+        "Tempat           : Kediaman mempelai wanita di Palembang\n\n"
+        "Resepsi (Tueng Dara Baro) di Banda Aceh\n"
+        "Hari, tanggal    : Minggu, 17 November 2024\n"
+        "Waktu            : 11:30 WIB s.d. Selesai\n"
+        "Tempat           : Gedung Hj. Yusriah di Banda Aceh\n\n"
+        f"Berikut link untuk info lengkap dari acara kami :\n"
+        f"https://foreverr.id/alvioalvin/?to={encoded_name}\n\n"
+        "Merupakan suatu kebahagiaan bagi kami apabila Bapak/Ibu/Saudara/i berkenan untuk hadir dan memberikan doa restu.\n\n"
+        "Mohon maaf perihal undangan hanya dibagikan melalui pesan ini dikarenakan keterbatasan jarak dan waktu.\n\n"
+        "Wassalamu'alaikum Warahmatullahi Wabarakaatuh\n\n"
+        "Terima Kasih..\n\n"
+        "Hormat kami,\n"
+        "Alvio & Alvin"
+    )
     pyautogui.click(588, 479)
     pyautogui.hotkey('ctrl', 'a')
     pyautogui.press('backspace')
@@ -29,5 +59,6 @@ def send_whatsapp_message(name, phone):
 # Loop through each contact and send message
 for index, row in contacts.iterrows():
     name = row['name']
+    title = row['title']
     phone = row['phone']
-    send_whatsapp_message(name, phone)
+    send_whatsapp_message(name, title, phone)
